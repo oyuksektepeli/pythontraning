ef3f1753aba2f092b5154b9f468f87b017ffa462
Onur YUKSEKTEPELI <onuryuksektepeli@hotmail.com.tr>
2018-10-29 17:12:26 +0300

    DataTypes

---
+++ DataTypes.py
@@ -0,0 +1,214 @@
+
+# coding: utf-8
+
+# # Python Veri Tipleri
+
+# Integers(int)---> 22, -111, 0, 9000, 
+# 
+# Real Numbers(float)---> 3.5, 0.0008, -50.5, 6.0; 
+# 
+# Characters(str)--->"Fiyat", "234", "$$$"; 
+
+# In[13]:
+
+
+type(55)
+
+
+# In[14]:
+
+
+type(5.5)
+
+
+# In[15]:
+
+
+type("Fiyat")
+
+
+# In[16]:
+
+
+type("55")
+
+
+# In[17]:
+
+
+2+3
+
+
+# Python Değişkenlerinde Kullanamayacağınız kelimeler
+# https://docs.python.org/2.5/ref/keywords.html
+
+# In[ ]:
+
+
+Python Değişken isimlerinde dikkat edilmesi gereken kurallar
+https://docs.python.org/2.5/ref/identifiers.html
+
+
+# In[18]:
+
+
+sayi1 = 5
+
+
+# In[19]:
+
+
+sayi1
+
+
+# In[20]:
+
+
+sayi2
+
+
+# In[21]:
+
+
+6 = sayi2
+
+
+# In[22]:
+
+
+sayi1
+
+
+# In[23]:
+
+
+sayi1="Fiyat"
+
+
+# In[24]:
+
+
+sayi1
+
+
+# In[25]:
+
+
+1Sayi
+
+
+# In[26]:
+
+
+int sayi1
+
+
+# In[27]:
+
+
+int sayi1 = 5
+
+
+# In[28]:
+
+
+type(sayi1)
+
+
+# In[29]:
+
+
+sayi1,sayi2,sayi3 = 1,2,3
+
+
+# In[30]:
+
+
+sayi1
+
+
+# In[31]:
+
+
+sayi2
+
+
+# In[32]:
+
+
+sayi3
+
+
+# In[33]:
+
+
+Name,Age,Gender = "Onur",34,"Male"
+
+
+# In[34]:
+
+
+Name
+Age
+Gender
+
+
+# In[35]:
+
+
+Age
+
+
+# In[36]:
+
+
+Gender
+
+
+# In[37]:
+
+
+sayi1 + sayi2
+
+
+# In[38]:
+
+
+print("sayi1")
+
+
+# In[39]:
+
+
+import math
+
+
+# In[40]:
+
+
+math.pi
+
+
+# In[41]:
+
+
+math.sqrt(4)
+
+
+# In[42]:
+
+
+2**2
+
+
+# In[43]:
+
+
+deger = input("Sayi Giriniz: ")
+
+
+# In[44]:
+
+
+deger
+
