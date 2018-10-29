9d62e32108a5e2e607401865b8ebc08e07782abd
Onur YUKSEKTEPELI <onuryuksektepeli@hotmail.com.tr>
2018-10-29 17:17:47 +0300

    Numbers

---
+++ FormatingNumbers.py
@@ -0,0 +1,142 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+a=1234.99
+
+
+# In[2]:
+
+
+b=0.3
+
+
+# In[3]:
+
+
+c=7
+
+
+# In[4]:
+
+
+x=a*b*c
+
+
+# In[5]:
+
+
+x
+
+
+# In[6]:
+
+
+round(x,2)
+
+
+# In[7]:
+
+
+round(x,4)
+
+
+# In[8]:
+
+
+format(x,".4f")
+
+
+# width.precisionf
+
+# In[9]:
+
+
+format(x,"20.4f")
+
+
+# In[10]:
+
+
+format(x,"3.4f")
+
+
+# In[11]:
+
+
+format(x,".2f")
+
+
+# In[12]:
+
+
+format(x,".2e")
+
+
+# In[13]:
+
+
+format(x,".4E")
+
+
+# In[14]:
+
+
+format(9823094823094823,"5,.2f")
+
+
+# In[15]:
+
+
+format(0.78456,"%")
+
+
+# In[16]:
+
+
+format(0.9879878,"10.2%")
+
+
+# In[17]:
+
+
+format(78,"5d")
+
+
+# In[18]:
+
+
+format(5,"b")
+
+
+# In[19]:
+
+
+format(99,"b")
+
+
+# In[20]:
+
+
+format(99,"x")
+
+
+# In[21]:
+
+
+format(4,"x")
+
+
+# In[22]:
+
+
+format(11,"x")
+
+
+# In[23]:
+
+
+format(11,"o")
+
---
+++ Numbers.py
@@ -0,0 +1,172 @@
+
+# coding: utf-8
+
+# #Python Sayılar
+
+# In[3]:
+
+
+type(2)
+
+
+# In[4]:
+
+
+type(2.0)
+
+
+# In[5]:
+
+
+type(2f)
+
+
+# In[6]:
+
+
+type(2j)
+
+
+# In[7]:
+
+
+2+9
+
+
+# In[8]:
+
+
+9-8
+
+
+# In[9]:
+
+
+8*8
+
+
+# In[10]:
+
+
+3/2
+
+
+# In[11]:
+
+
+8**3
+
+
+# In[12]:
+
+
+4**0.5
+
+
+# Complex Number
+# 2+3j
+
+# In[13]:
+
+
+type(5j)
+
+
+# In[14]:
+
+
+abs(100)
+
+
+# In[15]:
+
+
+abs(-100)
+
+
+# In[16]:
+
+
+pow(2,3)
+
+
+# In[17]:
+
+
+round(2.6)
+
+
+# In[18]:
+
+
+round(2.3)
+
+
+# In[19]:
+
+
+round(2.123456,3)
+
+
+# In[20]:
+
+
+min(5,90,3)
+
+
+# In[21]:
+
+
+max(5,90,6)
+
+
+# In[22]:
+
+
+import math
+
+
+# In[23]:
+
+
+math.pi
+
+
+# In[24]:
+
+
+math.e
+
+
+# In[25]:
+
+
+math.ceil(3.4)
+
+
+# In[26]:
+
+
+math.floor(3.4)
+
+
+# In[27]:
+
+
+math.sqrt(9)
+
+
+# In[28]:
+
+
+math.log(4)
+
+
+# In[29]:
+
+
+math.sin(2)
+
+
+# Pyhton Matematik Modülü
+# https://docs.python.org/3.4/library/math.html
