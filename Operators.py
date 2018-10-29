578196cb021fc7cc79173a1cb7c17214191a7abd
Onur YUKSEKTEPELI <onuryuksektepeli@hotmail.com.tr>
2018-10-29 17:19:51 +0300

    Training Files

---
+++ Operators.py
@@ -0,0 +1,328 @@
+
+# coding: utf-8
+
+# In[12]:
+
+
+a=2
+
+
+# In[13]:
+
+
+b=3
+
+
+# In[14]:
+
+
+a+b
+
+
+# In[15]:
+
+
+b/a
+
+
+# In[16]:
+
+
+b//a
+
+
+# In[17]:
+
+
+b*a
+
+
+# In[18]:
+
+
+b**a
+
+
+# In[19]:
+
+
+5%2
+
+
+# Operator Oncelikleri
+# https://docs.python.org/3/reference/expressions.html#id21
+
+# In[20]:
+
+
+10*5+9
+
+
+# In[21]:
+
+
+5+12/2*4
+
+
+# In[22]:
+
+
+x=5
+
+
+# In[23]:
+
+
+x
+
+
+# In[24]:
+
+
+x=x+1
+
+
+# In[25]:
+
+
+x
+
+
+# In[26]:
+
+
+x+=5
+
+
+# In[27]:
+
+
+x
+
+
+# In[28]:
+
+
+x-=3
+
+
+# In[29]:
+
+
+x
+
+
+# In[30]:
+
+
+x*=2
+
+
+# In[31]:
+
+
+x
+
+
+# In[32]:
+
+
+x%=4
+
+
+# In[33]:
+
+
+x
+
+
+# In[34]:
+
+
+5+5
+
+
+# In[35]:
+
+
+5+5.0
+
+
+# In[36]:
+
+
+5.3 + 6.7
+
+
+# In[37]:
+
+
+int(2.7)
+
+
+# In[38]:
+
+
+float(44)
+
+
+# In[39]:
+
+
+str(44)
+
+
+# In[40]:
+
+
+var1 = True
+
+
+# In[41]:
+
+
+var2 = False
+
+
+# In[42]:
+
+
+type(var1)
+
+
+# In[43]:
+
+
+type(True)
+
+
+# In[44]:
+
+
+type(False)
+
+
+# In[45]:
+
+
+bool(12)
+
+
+# In[46]:
+
+
+bool(34.3)
+
+
+# In[47]:
+
+
+bool("")
+
+
+# In[48]:
+
+
+bool(0)
+
+
+# In[49]:
+
+
+3<4
+
+
+# In[50]:
+
+
+4<3
+
+
+# In[51]:
+
+
+4>3
+
+
+# In[52]:
+
+
+11<=10
+
+
+# In[53]:
+
+
+10<=11
+
+
+# In[54]:
+
+
+10<=10
+
+
+# In[55]:
+
+
+2==2
+
+
+# In[56]:
+
+
+2!=2
+
+
+# In[57]:
+
+
+2!=1
+
+
+# AND Operatoru
+# False AND False = False
+# False AND True = False 
+# True AND False = False
+# True AND True = True
+
+# In[58]:
+
+
+(10<19) and (2<9)
+
+
+# In[59]:
+
+
+(19<10) and (2<9)
+
+
+# OR Operatoru
+# False OR False = False
+# False OR True = True
+# True OR False = True
+# True OR True = True
+
+# In[60]:
+
+
+(8<7) or (8<10)
+
+
+# NOT Operatoru
+# Operand;
+# True => False 
+# FAlse => True 
+
+# In[61]:
+
+
+not (200=200)
+
+
+# In[62]:
+
+
+not (200==200)
+
