4603fb318e1eafbb6a4d0f9570092b9b6c0b24a0
oyuksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-05-04 19:38:41 +0300

    python strings

---
+++ Strings.py
@@ -0,0 +1,60 @@
+s1 = "Merhaba dünya"
+s2 = 'Merhaba dünya'
+print(s1)
+print(s2)
+
+len(s1)
+len(s2)
+
+s3 = "a"
+type(s3)
+
+s4 = ""
+s4
+s5 = ''
+s5
+type(s4)
+type(s5)
+
+print("I'm learnig python")
+print('I'm learning python')
+
+s6 = "Selam\tOnur\tNasılsın"
+print(s6)
+
+s7 = "Selam\nOnur\nNasılsın"
+print(s7)
+
+s8 = "Selam "
+
+s9 = "Onur"
+
+s8 + s9
+
+s10 = "Selam " + "Onur"
+print(s10)
+
+s11 = "Selam" + 100
+
+s = "Onur " * 5
+print(s)
+
+s12 = "Selam Onur"
+
+"Selam" in s12
+
+s13 = "Selam"
+
+s13[6]
+
+s13[-1]
+
+s13[0:2]
+
+s13[:]
+
+s14 = "selam"
+
+s14.upper()
+
+s14.lower()
